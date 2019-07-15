c92938825e6a50a86fe16c2820ae912aec79ec91
Damián Pumar <damianpumar@gmail.com>
2019-07-15 14:21:32 +0200

    Change for by foreach

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,46 +8,46 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!this.isAgedBrie(items[i]) && !this.isBackStage(items[i])) {
-                if (items[i].quality > 0) {
-                    if (!this.isSulfuras(items[i])) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : this.items) {
+            if (!this.isAgedBrie(item) && !this.isBackStage(item)) {
+                if (item.quality > 0) {
+                    if (!this.isSulfuras(item)) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (this.isBackStage(items[i])) {
-                        if (items[i].sellIn < 11) {
-                            increaseQuality(items[i]);
+                    if (this.isBackStage(item)) {
+                        if (item.sellIn < 11) {
+                            increaseQuality(item);
                         }
 
-                        if (items[i].sellIn < 6) {
-                            increaseQuality(items[i]);
+                        if (item.sellIn < 6) {
+                            increaseQuality(item);
                         }
                     }
                 }
             }
 
-            if (!this.isSulfuras(items[i])) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!this.isSulfuras(item)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!this.isAgedBrie(items[i])) {
-                    if (!this.isBackStage(items[i])) {
-                        if (items[i].quality > 0) {
-                            if (!this.isSulfuras(items[i])) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!this.isAgedBrie(item)) {
+                    if (!this.isBackStage(item)) {
+                        if (item.quality > 0) {
+                            if (!this.isSulfuras(item)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    increaseQuality(items[i]);
+                    increaseQuality(item);
                 }
             }
         }
